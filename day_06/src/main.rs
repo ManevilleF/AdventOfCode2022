16a0b19ad395ff21c93b6bac7011f30bb9cbf47c
Felix de Maneville <felix.maneville@gmail.com>
2022-12-06 12:57:03 +0100

    cleanup

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashSet;
 
-fn find_marker(chars: &[char], window: usize) -> Option<usize> {
-    chars
+fn find_marker(str: &str, window: usize) -> Option<usize> {
+    str.as_bytes()
         .windows(window)
         .into_iter()
         .position(|items| {
-            let set: HashSet<char> = items.iter().copied().collect();
+            let set: HashSet<_> = items.iter().copied().collect();
             set.len() == window
         })
         .map(|p| p + window)
@@ -14,14 +14,7 @@ fn find_marker(chars: &[char], window: usize) -> Option<usize> {
 fn main() {
     let file = include_str!("../input.txt");
     for line in file.lines() {
-        let chars: Vec<_> = line.chars().collect();
-        println!(
-            "First marker position at {}",
-            find_marker(&chars, 4).unwrap()
-        );
-        println!(
-            "First message position at {}",
-            find_marker(&chars, 14).unwrap()
-        );
+        println!("First marker at {}", find_marker(line, 4).unwrap());
+        println!("First message at {}", find_marker(line, 14).unwrap());
     }
 }
