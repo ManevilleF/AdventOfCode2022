21332e66a2b7a7bbaab7b8a1d10920c26d488702
Felix de Maneville <felix.maneville@gmail.com>
2022-12-07 13:40:32 +0100

    Cleanup

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -4,82 +4,46 @@ const DISK_SPACE: usize = 70_000_000;
 const REQUIRED_SPACE: usize = 30_000_000;
 
 #[derive(Debug, Default)]
-struct Dir {
-    file_size: usize,
-    dirs: Dirs,
-}
+struct Dirs(pub HashMap<String, usize>);
 
-#[derive(Debug, Default)]
-struct Dirs(pub HashMap<String, Dir>);
-
-impl Dir {
-    pub fn total_size(&self) -> usize {
-        self.file_size + self.dirs.total_size()
-    }
-
-    pub fn push_entry(&mut self, entry: &str) -> Result<(), String> {
+impl Dirs {
+    pub fn push_entry(&mut self, path: &[String], entry: &str) -> Result<(), String> {
         let (prefix, name) = entry
             .split_once(' ')
             .ok_or_else(|| format!("`{entry}` is not a valid entry"))?;
-        match prefix {
-            "dir" => {
-                self.dirs.0.insert(name.to_string(), Dir::default());
-            }
-            _ => {
-                let size: usize = prefix
-                    .parse()
-                    .map_err(|_| format!("{prefix} is not a valid file size"))?;
-                self.file_size += size;
+        if prefix == "dir" {
+            let path_str = format!("{}{name}", path.join(""));
+            self.0.entry(path_str).or_insert(0);
+        } else {
+            let size: usize = prefix
+                .parse()
+                .map_err(|_| format!("{prefix} is not a valid file size"))?;
+            let mut dir_path = String::new();
+            for p in path {
+                dir_path.push_str(p);
+                *self.0.entry(dir_path.clone()).or_default() += size;
             }
         }
         Ok(())
-    }
-}
-
-impl Dirs {
-    pub fn get_dir_mut(&mut self, depth: &[String]) -> Option<&mut Dir> {
-        let dir = self.0.get_mut(&depth[0])?;
-        if depth.len() > 1 {
-            dir.dirs.get_dir_mut(&depth[1..])
-        } else {
-            Some(dir)
-        }
-    }
-
-    pub fn total_size(&self) -> usize {
-        self.0.values().map(Dir::total_size).sum()
     }
 
     pub fn part1(&self) -> usize {
         self.0
             .values()
-            .map(|dir| {
-                let total_size = dir.total_size();
-                let size = if total_size <= 100_000 { total_size } else { 0 };
-                size + dir.dirs.part1()
-            })
+            .copied()
+            .filter(|size| *size <= 100_000)
             .sum()
     }
 
-    pub fn all_sizes(&self) -> Vec<usize> {
-        self.0
-            .values()
-            .flat_map(|dir| {
-                let mut sizes = vec![dir.total_size()];
-                sizes.extend(dir.dirs.all_sizes());
-                sizes
-            })
-            .collect()
-    }
-
     pub fn part2(&self) -> usize {
-        let available_space = DISK_SPACE.saturating_sub(self.total_size());
+        let available_space = DISK_SPACE.saturating_sub(self.0["/"]);
         let expected_size = REQUIRED_SPACE.saturating_sub(available_space);
-        let mut sizes = self.all_sizes();
+        let mut sizes: Vec<_> = self.0.values().collect();
         sizes.sort_unstable();
         sizes
             .into_iter()
-            .find(|size| *size >= expected_size)
+            .find(|size| **size >= expected_size)
+            .copied()
             .unwrap()
     }
 }
@@ -89,24 +53,20 @@ impl FromStr for Dirs {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut dirs = Self::default();
-        dirs.0.insert("/".to_string(), Dir::default());
-
-        let mut current_depth = vec![];
+        let mut current_path = Vec::new();
 
         for section in s.lines() {
             match section {
                 "$ ls" => {}
                 "$ cd .." => {
-                    current_depth.pop();
+                    current_path.pop();
                 }
                 section if section.starts_with("$ cd ") => {
-                    current_depth.push(section[5..].to_string());
+                    let dir_name = section[5..].to_string();
+                    current_path.push(dir_name);
                 }
                 _ => {
-                    let dir = dirs.get_dir_mut(&current_depth).ok_or_else(|| {
-                        format!("There is no valid directory at {}", current_depth.join("/"))
-                    })?;
-                    dir.push_entry(section)?;
+                    dirs.push_entry(&current_path, section)?;
                 }
             }
         }
@@ -118,7 +78,6 @@ fn main() {
     let input = include_str!("../input.txt");
     let hierarchy = Dirs::from_str(input).unwrap();
     // println!("{hierarchy:#?}");
-    println!("total size {}", hierarchy.total_size());
     println!("Part 1: {}", hierarchy.part1());
     println!("Part 2: {}", hierarchy.part2());
 }
